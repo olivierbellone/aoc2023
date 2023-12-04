782b605fe014b3e80520932b1ed6ef7a646c3c1c
Olivier Bellone <14116796+olivierbellone@users.noreply.github.com>
2023-12-03 21:34:02 -0800

    day 4

--- lib/aoc2023/day4.rb
+++ lib/aoc2023/day4.rb
@@ -9,19 +9,48 @@ module AoC2023
   class Day4 < Solver
     extend T::Sig
 
-    # sig { override.params(input: String).void }
-    # def initialize(input)
-    #   super
-    # end
+    sig { override.params(input: String).void }
+    def initialize(input)
+      super
+
+      @cards = T.let(
+        @input
+          .strip
+          .lines
+          .map do |line|
+            m = line.match(/^Card\W+(?<index>\d+): (?<winning_numbers>.*) \| (?<numbers>.*)$/)
+
+            raise "Unexpected input: #{line}" if m.nil?
+
+            winning_numbers = T.must(m["winning_numbers"]).split(" ").map { |x| x.strip.to_i }.to_set
+            numbers = T.must(m["numbers"]).split(" ").map { |x| x.strip.to_i }.to_set
+            [winning_numbers, numbers]
+          end,
+        T::Array[[T::Set[Integer], T::Set[Integer]]],
+      )
+
+      @scores = T.let(
+        @cards.map { |(winning_numbers, numbers)| winning_numbers.intersection(numbers).size },
+        T::Array[Integer],
+      )
+    end
 
     sig { override.returns(String) }
     def part1
-      ""
+      @scores.sum { |score| score == 0 ? 0 : 2**(score - 1) }.to_s
     end
 
     sig { override.returns(String) }
     def part2
-      ""
+      copies = @scores.map { 1 }
+
+      @scores.each_with_index do |score, index|
+        (1..score).each do |i|
+          copies[index + i] = copies.fetch(index + i) + copies.fetch(index)
+        end
+      end
+
+      copies.sum.to_s
     end
   end
 end
--- test/aoc2023/day4_test.rb
+++ test/aoc2023/day4_test.rb
@@ -6,19 +6,30 @@ require "test_helper"
 module AoC2023
   class Day4Test < Minitest::Test
     describe AoC2023::Day4 do
+      before do
+        @input = <<~INPUT
+          Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+          Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+          Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+          Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+          Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+          Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+        INPUT
+        @input = T.let(@input, String)
+      end
       describe "#part1" do
         it "solves part 1" do
-          solver = AoC2023::Day4.new("")
+          solver = AoC2023::Day4.new(@input)
 
-          assert_equal("", solver.part1)
+          assert_equal("13", solver.part1)
         end
       end
 
       describe "#part2" do
         it "solves part 2" do
-          solver = AoC2023::Day4.new("")
+          solver = AoC2023::Day4.new(@input)
 
-          assert_equal("", solver.part2)
+          assert_equal("30", solver.part2)
         end
       end
     end
